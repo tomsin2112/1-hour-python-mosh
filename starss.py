24ac76fdd12b1ca2378ec490a55bba142805672f
tomsin2112 <143127215+tomsin2112@users.noreply.github.com>
2023-09-06 14:54:54 +0100

    Add files via upload

---
+++ starss.py
@@ -0,0 +1,27 @@
+name= input("what is your name ? ")
+print("hello " + name)
+
+first=input("First" )
+second=input("second" )
+Answer=int(first) + int(second)
+print(Answer)
+
+numbers=[1, 2, 3, 4, 5, 6, 7, 9, 0, 10]
+sum_of_numbers= sum(numbers)
+print("sum of the first 10 integers= ",sum_of_numbers)
+
+product_of_numbers = 1
+for i in range(1, 10):
+    product_of_numbers *= i
+print("product od the first 10 integers=", product_of_numbers)
+
+
+
+
+
+
+
+
+
+
+

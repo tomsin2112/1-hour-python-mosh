1d5ca81a162d1d30631a0af8bde135da11e79584
tomsin2112 <143127215+tomsin2112@users.noreply.github.com>
2023-10-24 22:48:08 +0100

    Add files via upload

---
+++ statements.py
@@ -0,0 +1,26 @@
+age=19
+if age > 18:
+    print("YOU ARE WELCOME")
+
+if age< 18:
+    print("SORRY REQUIREMENTS NOT MET !!!")
+
+number=10
+if number > 0:
+    print("positive number")
+else:
+    print("negative number")
+
+scores = 30
+if scores > 50:
+    print("EXCELLENT!GOOD JOB")
+
+elif scores == 50:
+    print("AVERAGE,  CAN DO BETTER")
+
+else:
+    print("BELOW AVERAGE,MORE ROOM FOR IMPROVEMENT")
+
+
+
+
